721924a37bf7dc93087146446d10fed52a4e6411
Jake Lawrence <jakesamuellawrence@gmail.com>
2022-11-14 12:59:51 +0000

    Implement Displaying uploaded file contents  - Create FileUpload component  - Create ShowSource component  - Create Source class

---
+++ SigmaEmu.Core/Models/Source.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace SigmaEmu.Core.Models;
+
+public class Source
+{
+    public List<string> Lines { get; }
+
+    public Source(List<string> lines)
+    {
+        Lines = lines;
+    }
+
+    public static async Task<Source> FromFileAsync(IBrowserFile file)
+    {
+        var lines = new List<string>();
+        var fileReader = new StreamReader(file.OpenReadStream());
+        
+        string? line;
+        while ((line = await fileReader.ReadLineAsync()) != null)
+        {
+            lines.Add(line);
+        }
+
+        return new Source(lines);
+    }
+}
