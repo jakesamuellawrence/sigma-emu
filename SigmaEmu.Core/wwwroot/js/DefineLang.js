4913e340295321f2da290fe2bebfb3b6f98ed3f4
Jake Lawrence <jakesamuellawrence@gmail.com>
2023-02-11 14:59:22 +0000

    Setup syntax highlighting

---
+++ SigmaEmu.Core/wwwroot/js/DefineLang.js
@@ -0,0 +1,23 @@
+﻿let keywords = ["lea", "load"]
+
+blazorMonaco.editor.registerSigma16 = function () {
+    monaco.languages.register({
+        id: "Sigma16"
+    });
+
+    monaco.languages.setMonarchTokensProvider("Sigma16", {
+        keywords,
+        tokenizer: {
+            root: [
+                [/@?[a-zA-Z][\w$]*/, {
+                    cases: {
+                        '@keywords': 'keyword',
+                        '@default': 'label'
+                    }
+                }],
+                [/;.*/, 'comment'],
+            ]
+        }
+    })
+}
+    
