067fb135691c20c801ad0761002a45d4d5e1d01a
Jake Lawrence <Jake Lawrence>
2023-02-11 19:24:31 +0000

    Fully implement syntax highlighting

--- SigmaEmu.Core/wwwroot/js/DefineLang.js
+++ SigmaEmu.Core/wwwroot/js/DefineLang.js
@@ -1,4 +1,21 @@
-﻿let keywords = ["lea", "load"]
+﻿let mnemonics = [
+    "add", "sub", "mul", "div",
+    "cmplt", "cmpeq", "cmpgt",
+    "inv", "and", "or", "xor", 
+    "shiftl", "shiftr", "trap",
+    "lea", "load", "store",
+    "jumpf", "jumpt", "jal", "jump",
+    "data"
+]
+
+let registers = [
+    "R0", "R1", "R2", "R3",
+    "R4", "R5", "R6", "R7",
+    "R8", "R9", "R10", "R11",
+    "R12", "R13", "R14", "R15",
+]
+
+let dataKeyword = ["data"]
 
 blazorMonaco.editor.registerSigma16 = function () {
     monaco.languages.register({
@@ -6,13 +23,20 @@ blazorMonaco.editor.registerSigma16 = function () {
     });
 
     monaco.languages.setMonarchTokensProvider("Sigma16", {
-        keywords,
+        mnemonics,
+        registers,
+        dataKeyword,
+        ignoreCase: true,
         tokenizer: {
             root: [
+                [/[\[\]]/, 'string.value.json'],
+                [/\$[0-9a-f]{4}/, 'string'],
+                [/[0-9]+/, 'number'],
                 [/@?[a-zA-Z][\w$]*/, {
                     cases: {
-                        '@keywords': 'keyword',
-                        '@default': 'label'
+                        '@mnemonics': 'type',
+                        '@registers': 'annotation',
+                        '@default': 'variable',
                     }
                 }],
                 [/;.*/, 'comment'],
