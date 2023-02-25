d066661fa72588db46f6ee4d597bd930d44779e7
Jake Lawrence <jakesamuellawrence@gmail.com>
2023-02-25 14:39:51 +0000

    Better state display names on Processor Status Panel

--- SigmaEmu.Core/Models/ProcessorRunningState.cs
+++ SigmaEmu.Core/Models/ProcessorRunningState.cs
@@ -8,3 +8,23 @@ public enum ProcessorRunningState
     Stopped,
     Paused
 }
+
+public static class ProcessorRunningStateExtensions
+{
+    public static string ForDisplay(this ProcessorRunningState state)
+    {
+        switch (state)
+        {
+            case ProcessorRunningState.Playing:
+            case ProcessorRunningState.PlayingFast:
+            case ProcessorRunningState.PlayingUnbounded:
+                return "Playing";
+            case ProcessorRunningState.Stopped:
+                return "Stopped";
+            case ProcessorRunningState.Paused:
+                return "Paused";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(state), state, null);
+        }
+    }
+}
